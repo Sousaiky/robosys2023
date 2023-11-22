e73e65b1c9f977c0a00e4b4a0217b57e5b9c7670
Soshi Saiki <sousaiky@yahoo.com>
2023-11-22 18:31:07 +0900

    add command

--- test.bash
+++ test.bash
@@ -10,8 +10,45 @@ ng () {
 res=0
 
 ### I/O ###
+out=$(seq 1 | ./cit1)
+[ "${out1}" = 1] || ng ${LINENO}
+[ "${out2}" = "奇数です"] || ng ${LINENO}
+
+out=$(seq 2 | ./cit1)
+[ "${out1}" = 3] || ng ${LINENO}
+[ "${out2}" = "奇数です"] || ng ${LINENO}
+
+out=$(seq 3 | ./cit1)
+[ "${out1}" = 6] || ng ${LINENO}
+[ "${out2}" = "偶数です"] || ng ${LINENO}
+
+out=$(seq 4 | ./cit1)
+[ "${out1}" = 10] || ng ${LINENO}
+[ "${out2}" = "偶数です"] || ng ${LINENO}
+
 out=$(seq 5 | ./cit1)
-[ "${out}" = 15] || ng ${LINENO}
+[ "${out1}" = 15] || ng ${LINENO}
+[ "${out2}" = "奇数です" || ng ${LINENO}
+
+out=$(seq 6 | ./cit1)
+[ "${out1}" = 21] || ng ${LINENO}
+[ "${out2}" = "奇数です" || ng ${LINENO}
+
+out=$(seq 7 | ./cit1)
+[ "${out1}" = 28] || ng ${LINENO}
+[ "${out2}" = "偶数です" || ng ${LINENO}
+
+out=$(seq 8 | ./cit1)
+[ "${out1}" = 36] || ng ${LINENO}
+[ "${out2}" = "奇数です" || ng ${LINENO}
+
+out=$(seq 9 | ./cit1)
+[ "${out1}" = 45] || ng ${LINENO}
+[ "${out2}" = "奇数です" || ng ${LINENO}
+
+out=$(seq 10 | ./cit1)
+[ "${out1}" = 55] || ng ${LINENO}
+[ "${out2}" = "奇数です" || ng ${LINENO}
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./cit1)
