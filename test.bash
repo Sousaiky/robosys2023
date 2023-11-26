bda0515efe03f15549fbf42c7f6affab6e954287
Soshi Saiki <sousaiky@yahoo.com>
2023-11-26 21:28:00 +0900

    add comment

--- test.bash
+++ test.bash
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 #!/bin/bash
-
 ng () {
     echo "NG at Line $1"
     res=1
@@ -13,65 +12,41 @@ res=0
 
 ### I/O ###
 out=$(seq 1 | ./cit1)
-[ "${out}" = 1 ] || ng ${LINENO}
-out2=$(seq 1 | ./cit1 | tail -n 1)
-[ "${out2}" = "奇数です" ] || ng ${LINENO}
+[ "${out}" == "1 奇数です" ] || ng ${LINENO}
 
 out=$(seq 2 | ./cit1)
-[ "${out}" = 3 ] || ng ${LINENO}
-out2=$(seq 2 | ./cit1 | tail -n 1)
-[ "${out2}" = "奇数です" ] || ng ${LINENO}
+[ "${out}" == "3 奇数です" ] || ng ${LINENO}
 
 out=$(seq 3 | ./cit1)
-[ "${out}" = 6 ] || ng ${LINENO}
-out2=$(seq 3 | ./cit1 | tail -n 1)
-[ "${out2}" = "偶数です" ] || ng ${LINENO}
+[ "${out}" == "6 偶数です" ] || ng ${LINENO}
 
 out=$(seq 4 | ./cit1)
-[ "${out}" = 10 ] || ng ${LINENO}
-out2=$(seq 4 | ./cit1 | tail -n 1)
-[ "${out2}" = "偶数です" ] || ng ${LINENO}
+[ "${out}" == "10 偶数です" ] || ng ${LINENO}
 
 out=$(seq 5 | ./cit1)
-[ "${out}" = 15 ] || ng ${LINENO}
-out2=$(seq 5 | ./cit1 | tail -n 1)
-[ "${out2}" = "奇数です" ] || ng ${LINENO}
+[ "${out}" == "15 奇数です" ] || ng ${LINENO}
 
 out=$(seq 6 | ./cit1)
-[ "${out}" = 21 ] || ng ${LINENO}
-out2=$(seq 6 | ./cit1 | tail -n 1)
-[ "${out2}" = "奇数です" ] || ng ${LINENO}
+[ "${out}" == "21 奇数です" ] || ng ${LINENO}
 
 out=$(seq 7 | ./cit1)
-[ "${out}" = 28 ] || ng ${LINENO}
-out2=$(seq 7 | ./cit1 | tail -n 1)
-[ "${out2}" = "偶数です" ] || ng ${LINENO}
+[ "${out}" == "28 偶数です" ] || ng ${LINENO}
 
 out=$(seq 8 | ./cit1)
-[ "${out}" = 36 ] || ng ${LINENO}
-out2=$(seq 8 | ./cit1 | tail -n 1)
-[ "${out2}" = "奇数です" ] || ng ${LINENO}
+[ "${out}" == "36 偶数です" ] || ng ${LINENO}
 
 out=$(seq 9 | ./cit1)
-[ "${out}" = 45 ] || ng ${LINENO}
-out2=$(seq 9 | ./cit1 | tail -n 1)
-[ "${out2}" = "奇数です" ] || ng ${LINENO}
+[ "${out}" == "45 奇数です" ] || ng ${LINENO}
 
 out=$(seq 10 | ./cit1)
-[ "${out}" = 55 ] || ng ${LINENO}
-out2=$(seq 10 | ./cit1 | tail -n 1)
-[ "${out2}" = "奇数です" ] || ng ${LINENO}
+[ "${out}" == "55 奇数です" ] || ng ${LINENO}
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./cit1)
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" != 0 ] || ng ${LINENO}
 
 out=$(echo | ./cit1) #空文字
-[ "$?" = 1 ] || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" != 0 ] || ng ${LINENO}
 
-[ "$res" = 0 ] && echo "OK"
+[ "$res" == 0 ] && echo "OK"
 exit $res
-
-
